d3fe799f80f857c16e2277e7bea62afe068ea5fa
Margherita Pecorelli <margherita.pecorelli@studio.unibo.it>
2016-01-18 18:25:55 +0100

    Added class ToBuy

---
+++ src/it/unibo/monopoli/model/actions/ToBuy.java
@@ -0,0 +1,18 @@
+package it.unibo.monopoli.model.actions;
+
+import it.unibo.monopoli.model.mainunits.Player;
+import it.unibo.monopoli.model.table.Building;
+import it.unibo.monopoli.model.table.Ownership;
+
+/**
+ * This class represent on of the {@link Action}s of the game.
+ * This one allows to buy {@link Ownership}s and/or {@link Building}s.
+ *
+ */
+public class ToBuy implements Action {
+
+    @Override
+    public void play(final Player player) {
+    }
+
+}

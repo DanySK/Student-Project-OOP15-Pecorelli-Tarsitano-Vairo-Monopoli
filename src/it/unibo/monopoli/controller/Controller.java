35b7f00bff1a5c119c63379ec4997bc21c3c7781
Giuseppe Vairo <giuseppe.vairo@studio.unibo.it>
2016-01-25 14:08:15 +0100

    added method on ControllerImpl

--- src/it/unibo/monopoli/controller/Controller.java
+++ src/it/unibo/monopoli/controller/Controller.java
@@ -13,21 +13,8 @@ public interface Controller {
     * @param strategy
     * - set a strategy {@link JTextField}s
     */
-    void setStrategy(JTextField strategy);
+    void setStrategy();
 
-    /**
-    * add player in a list.
-    * @param name .
-    * @param idPawn .
-    * @param typePlayer .
-    */
-    void addPlayer(final JTextField name, final int idPawn, final int typePlayer);
-
-    /**
-    * Remove player from list.
-    * @param name .
-    */
-    void removePlayer(final JTextField name);
 
 
 }
